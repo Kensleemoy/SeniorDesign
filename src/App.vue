591909e5bf22916977cbd44e0d3928e006d4ede9
sbenda14 <susanbenda@u.boisestate.edu>
2020-09-16 13:44:37 -0600

    create employee form

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,20 @@
     <h1>Employees</h1>
 
     <!-- <employee-table />-->
+	<employee-form />
 	<employee-table :employees="employees" />
   </div>
 </template>
 
 <script>
   import EmployeeTable from '@/components/EmployeeTable.vue'
+  import EmployeeForm from '@/components/EmployeeForm.vue'
 
   export default {
     name: 'app',
     components: {
       EmployeeTable,
+	  EmployeeForm,
     },
 	data(){
 		return{
