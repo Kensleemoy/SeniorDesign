b0943d8548c3f85b7944c5f9dad68aaa7b42f825
sbenda14 <susanbenda@u.boisestate.edu>
2020-09-16 13:40:51 -0600

    changed table to use array

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
   <div id="app" class="small-container">
     <h1>Employees</h1>
 
-    <employee-table />
+    <!-- <employee-table />-->
+	<employee-table :employees="employees" />
   </div>
 </template>
 
@@ -15,7 +16,28 @@
     components: {
       EmployeeTable,
     },
-  }
+	data(){
+		return{
+			employees:[
+			{
+				id:1,
+				name: 'Richard Hendricks',
+				email: 'richard@piedpiper.com',
+			},
+			{
+				id: 2,
+				name: 'Bertram Gilfoyle',
+				email: 'gilfoyle@piedpiper.com',
+			},
+			{
+				id: 3,
+				name: 'Dinesh Chugtai',
+				email: 'dinesh@piedpiper.com',
+			},
+      ],
+    }
+  },
+}
 </script>
 
 <style>
