4cebfe97c04de0df13fec0b95b3627739b72d679
Kenslee Moy <Kensleemoy@u.boisestate.edu>
2020-10-15 13:18:13 -0600

    moving rules to global css

--- src/pages/About/App.vue
+++ src/pages/About/App.vue
@@ -3,6 +3,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
     <Header />
 	<h1>ABOUT US</h1>
+  <div class="horizontal-container">
+    <hr />
+  </div>
   </div>
 </template>
 
@@ -22,11 +25,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-/* This makes sure there is not weird padding around header */
-html, body {
-    margin: 0;
-    padding: 0;
 }
 </style>
