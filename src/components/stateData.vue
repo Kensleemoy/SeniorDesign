d9012852ede5cc91c4d8043237dfee0ae9706645
Kenslee Moy <Kensleemoy@u.boisestate.edu>
2020-10-15 14:41:52 -0600

    commented out data.actuals - issue surfacing it

--- src/components/stateData.vue
+++ src/components/stateData.vue
@@ -2,23 +2,22 @@
   <div id="stateData">
 	<div> Last updated on {{state.lastUpdatedDate}}</div>
 	<div>Population: {{state.population}}</div>
-	<div>Cases: {{state.actuals.cases}}</div>
-	<div>Deaths: {{state.actuals.deaths}}</div>
+	<!-- <div>Cases: {{this.state.actuals.cases}}</div>
+	<div>Deaths: {{this.state.actuals.deaths}}</div>
 	<div>Hospital Beds: {{state.actuals.hospitalBeds.capacity}}</div>
 	<div>Hospital beds used by Covid: {{state.actuals.hospitalBeds.currentUsageCovid}}</div>
 	<div>ICU Beds: {{state.actuals.icuBeds.capacity}}</div>
-	<div>ICU beds used by Covid: {{state.actuals.icuBeds.currentUsageCovid}}</div>
+	<div>ICU beds used by Covid: {{state.actuals.icuBeds.currentUsageCovid}}</div> -->
 	
   </div>
 </template>
 
 <script>
   export default {
-    name: 'stateData',
+    name: 'StateData',
 	props: {
-		state: Array,
+		state: Object,
 	},
-
   }
 </script>
 
--- src/pages/Home/App.vue
+++ src/pages/Home/App.vue
@@ -5,9 +5,7 @@
 		<div class="short-it">
 			<div class="body">
     <h1>Cases In Idaho</h1>
-    <stateData
-		:state="state" 
-	/>
+    <StateData :state="state" />
   </div>
 		</div>
 		<div />
@@ -16,17 +14,16 @@
 </template>
 
 <script>
-import stateData from '@/components/stateData.vue'
 
 export default {
 	name: 'Home',
 	components: {
 		Header: () => import('@/components/Header.vue'),
-		stateData,
+		StateData: () => import('@/components/stateData.vue'),
 	},
 	data(){
 		return{
-			state: [],
+			state: {},
 	}
 	},
 	mounted() {
@@ -37,11 +34,12 @@ export default {
 			try {
 				const proxyurl = "https://cors-anywhere.herokuapp.com/";
 				const url = "https://api.covidactnow.org/v2/state/ID.json?apiKey=bd6b09ad6c234b8aad900c39489909cf";
-				const response = await fetch (proxyurl + url)
-				const data = await response.json()
-				this.state = data
+				const response = await fetch (proxyurl + url);
+        const data = await response.json();
+        console.log(data);
+        this.state = data;
 			} catch (error) {
-				console.error(error)
+				console.error(error);
 			}
 		},
 	}
@@ -73,13 +71,11 @@ a {
 }
 
 .short-it div {
-  /* position: absolute; */
     bottom: 0;
     z-index: 1;
     display: block;
     width: 100%;
     height: 40px;
-    background-image: linear-gradient(to bottom, transparent, #fff);
 }
 
 /* This makes sure there is not weird padding around header */
