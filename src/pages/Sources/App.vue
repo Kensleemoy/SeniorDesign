775e2eeb5a04b3ac39643d078983459149d2228b
sbenda14 <susanbenda@u.boisestate.edu>
2020-11-02 13:50:40 -0700

    source styling

--- src/pages/Sources/App.vue
+++ src/pages/Sources/App.vue
@@ -6,15 +6,15 @@
     <div class="horizontal-container">
     <hr /> 
 	<div class="source">
-		Data Validity Statement: Data is presented directly from our API source. More information on their data sources can be found here:
+		<span class = "title">Data Validity Statement</span> Data is presented directly from our API source. More information on their data sources can be found here:
 		<a href="https://docs.google.com/presentation/d/1XmKCBWYZr9VQKFAdWh_D7pkpGGM_oR9cPjj-UrNdMJQ/edit#slide=id.g91040bbb51_413_0" target="_blank" rel="noopener">
 			Covid Act Now - data sources </a>
 	</div>
 	<div class="source">
-		API: <a href="https://covidactnow.org/resources#api" target="_blank" rel="noopener">covidactnow.org</a>
+		<span class = "title">API </span> <a href="https://covidactnow.org/resources#api" target="_blank" rel="noopener">covidactnow.org</a>
 	</div>
 	<div class="source">
-      Map: <a href="https://commons.wikimedia.org/wiki/File:Idaho_map_counties.svg" target="_blank" rel="noopener">Wikimedia: Idaho County Map</a>
+      <span class = "title">Map </span> <a href="https://commons.wikimedia.org/wiki/File:Idaho_map_counties.svg" target="_blank" rel="noopener">Wikimedia: Idaho County Map</a>
     </div>
     </div>
 	</div>
@@ -41,6 +41,11 @@ export default {
 }
 .horizontal-container{
 	text-align: left;
+}
+.title{
+	font-weight: bold;
+	color:#ff9900;
+	font-size: larger;
 }
 .source{
 	margin: 20px;
