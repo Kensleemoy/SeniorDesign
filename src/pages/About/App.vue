4801d4f21b604bb82ac77b9dd1afea124869f5e2
Kenslee Moy <Kensleemoy@u.boisestate.edu>
2020-11-05 14:02:14 -0700

    added pup

--- src/pages/About/App.vue
+++ src/pages/About/App.vue
@@ -13,7 +13,7 @@
 	</div>
   <div class="aboutus">
     <div id="kenslee">
-      <div id="kenslee-photo" class="personal-photo"> <img src="@/assets/pngwing.com.png"> </div>
+      <div id="kenslee-photo" class="personal-photo"> <img src="@/assets/kenslee_dog_pic.jpg"> </div>
       <div class="likes">
         <h3 class="subhead">KENSLEE</h3>
         Likes: Ice Cream and Puppies
